95f56382cb3a5af3b2d21e0140f20e306c9ec7c9
Enderson <enderson273014@gmail.com>
2025-04-26 18:52:25 -0400

    first commit

---
+++ src/Components/Header.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { FiMenu, FiX } from "react-icons/fi"; // Import hamburger & close icons
+import "./Header.css";
+
+const Header = function () {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <header className="header">
+      <span className="logo">EnGomez.Dev</span>
+
+      {/* Toggle Menu Button */}
+      <button className="menu-btn" onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? <FiX size={30} /> : <FiMenu size={30} />}{" "}
+        {/* Toggle between icons */}
+      </button>
+
+      {/* Navigation Menu */}
+      <nav className={`nav-menu ${menuOpen ? "open" : ""}`}>
+        <ul className="nav-links">
+          <li onClick={() => setMenuOpen(false)}>Inicio</li>
+          <li onClick={() => setMenuOpen(false)}>Acerca de mí</li>
+          <li onClick={() => setMenuOpen(false)}>Proyectos</li>
+          <li onClick={() => setMenuOpen(false)}>Contacto</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/Components/Main.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <footer className="main">
+      <div>Main</div>
+    </footer>
+  );
+}
