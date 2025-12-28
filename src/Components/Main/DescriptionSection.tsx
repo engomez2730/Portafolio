440459ebe87a583d9b94736f80ce6382121afad7
Enderson <enderson273014@gmail.com>
2025-12-28 19:34:30 -0400

    Enderson

--- src/Components/Main/DescriptionSection.tsx
+++ src/Components/Main/DescriptionSection.tsx
@@ -2,38 +2,21 @@ import { Typography } from "antd";
 
 const { Title, Paragraph } = Typography;
 
-const ParagraphStyles = {
-  fontSize: "1.4rem",
-  color: "#FFF", // Dark gray color
-  fontWeight: "100",
-  textAlign: "left" as const,
-  fontFamily: "'Story Script', cursive",
-};
-
 const DescriptionSection = () => (
   <div className="main-description">
     <Title
-      style={{
-        fontSize: "3.0rem",
-        color: "#FFF", // Dark gray color
-        fontWeight: "600",
-        fontFamily: "'Story Script', cursive",
-      }}
+      className="main-title"
     >
       Enderson Gómez
     </Title>
     <Title
       ellipsis
       level={2}
-      style={{
-        color: "#FFF", // Dark gray color
-        fontWeight: "400",
-        fontFamily: "'Story Script', cursive",
-      }}
+      className="main-subtitle"
     >
       Web Developer and SQL Programmer
     </Title>
-    <Paragraph style={ParagraphStyles}>
+    <Paragraph className="main-paragraph">
       I’m a Web Developer and SQL Programmer passionate about building modern, responsive applications and uncovering insights from data. I have experience in frontend development with React.js, TypeScript, and JavaScript, and backend development with Node.js, Express.js, and databases like MongoDB, SQL Server, and PostgreSQL. As a data analyst, I use Power BI and Python (pandas, matplotlib) to analyze data and create impactful visualizations. I enjoy combining technical skills with business understanding to deliver full-stack solutions that make a difference.
     </Paragraph>
   </div>
